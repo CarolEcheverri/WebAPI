ac507697121d170838b114c51a798f784fa0bcee
DIANA CAROLINA ECHEVERRI CADAVID <dianaecheverri310010@correo.itm.edu.co>
2023-10-18 11:56:10 -0500

    Crear base de datos

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+//Esta línea me crea el contexto de la BD a la hora de correr esta API
+//Funciones Anónimas (x => x....) Arrow Functions - Lambda Functions
+builder.Services.AddDbContext<DataBaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

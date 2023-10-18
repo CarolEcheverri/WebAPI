7eb373b641f396ea5892ea52623801140a7e8ef7
DIANA CAROLINA ECHEVERRI CADAVID <dianaecheverri310010@correo.itm.edu.co>
2023-10-18 01:58:37 -0500

    AuditBase y Country con DataAnnotation

--- WebAPI/WebAPI/DAL/Entities/AuditBase.cs
+++ WebAPI/WebAPI/DAL/Entities/AuditBase.cs
@@ -1,6 +1,13 @@
-﻿namespace WebAPI.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.DAL.Entities
 {
-    public class Class
+    public class AuditBase
     {
+        [Key] //DataAnnotation me sirve para definir que esta propiedad ID es una PK
+        [Required] //No permite nulos 
+        public Guid Id { get; set; } //Sera la PK para todas las tablas de mi BD
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set;}
     }
 }
--- WebAPI/WebAPI/DAL/Entities/Country.cs
+++ WebAPI/WebAPI/DAL/Entities/Country.cs
@@ -1,6 +1,12 @@
-﻿namespace WebAPI.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.DAL.Entities
 {
-    public class Country
+    public class Country : AuditBase
     {
+        [Display(Name = "País")] // Para yo pintar el nombre bien bonito en el FrontEnd
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")] //Longitud de caracteres máxima que esta propiedad me permite tener, ejem varchar(50)
+        [Required(ErrorMessage = "¡El campo {0} es obligatorio!")]
+        public string Name { get; set; } //varchar(50
     }
 }
